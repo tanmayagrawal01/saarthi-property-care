c6815107c75e32c84dd0058e628847922e6f83b2
demorepo <tanmayagrawal278@gmail.com>
2025-06-22 16:55:01 +0530

    updated schemas

--- models/schema/Property.js
+++ models/schema/Property.js
@@ -5,22 +5,6 @@ const PropertySchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true
-    },
-    address: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    city_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'City',const mongoose = require('mongoose');
-
-const PropertySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
       trim: true,
       set: v => v.toLowerCase()
     },
@@ -63,36 +47,4 @@ const PropertySchema = new mongoose.Schema(
 );
 PropertySchema.index({ user_id: 1 });
 PropertySchema.index({ city_id: 1 });
-module.exports = mongoose.model('Property', PropertySchema);
-
-      required: true
-    },
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    property_type: {
-      type: String,
-      enum: ['hotel', 'guesthouse', 'airbnb', 'pg', 'villa', 'homestay'],
-      required: true
-    },
-    photos: {
-      type: [String], // array of image URLs
-      default: []
-    },
-    description: {
-      type: String,
-      maxlength: 1000
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false
-    }
-  },
-  {
-    timestamps: true
-  }
-);
-
 module.exports = mongoose.model('Property', PropertySchema);
