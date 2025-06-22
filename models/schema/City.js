3636e07fe0a95bbb3e6ee850b5a8dc18e9eb78f9
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2025-06-22 16:44:09 +0530

    Update City.js

--- models/schema/City.js
+++ models/schema/City.js
@@ -5,7 +5,8 @@ const CitySchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
+      set: v => v.toLowerCase()
     },
     state: {
       type: String,
@@ -26,5 +27,7 @@ const CitySchema = new mongoose.Schema(
     timestamps: true
   }
 );
+
+CitySchema.index({ name: 1, state: 1, pincode: 1 }, { unique: true });
 
 module.exports = mongoose.model('City', CitySchema);
