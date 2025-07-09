b161673a037deba5f1cf68b6bff63045455c0ec1
demorepo <tanmayagrawal278@gmail.com>
2025-07-10 00:34:02 +0530

    Remove folder

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../auth/auth.controller');
-
-router.post('/login', authController.login);
-router.post('/refresh-token', authController.refreshToken);
-router.post('/logout', authController.logout);
-
-module.exports = router;
